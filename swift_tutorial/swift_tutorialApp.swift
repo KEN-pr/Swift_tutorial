a673cdf21a593d58e9cb88a76c1067eaa5244093
KEN-pr <kenprofes@gmail.com>
2021-05-22 21:59:23 +0900

    make local repository

---
+++ swift_tutorial/swift_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_tutorialApp.swift
+//  swift_tutorial
+//
+//  Created by 2Y on 2021/05/22.
+//
+
+import SwiftUI
+
+@main
+struct swift_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
