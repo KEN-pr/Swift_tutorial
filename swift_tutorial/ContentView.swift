a6f05dadbfef240f60b8b4c023d977b8b02f7c51
KEN-pr <kenprofes@gmail.com>
2021-05-24 00:19:59 +0900

    finish swiftUI essentials vol.1

--- swift_tutorial/ContentView.swift
+++ swift_tutorial/ContentView.swift
@@ -9,8 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            MapView()
+                .frame(height:300)
+                .ignoresSafeArea(edges: .top)
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading) {
+                Text("うまぴょい伝説")
+                    .font(.title)
+                HStack {
+                    Text("ウマ娘")
+                    Spacer()
+                    Text("プリティダービー")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+            Divider()
+                
+            Text("育成シミュレーションゲーム")
+                .font(.title2)
+            Text("presented by Cygames")
+            }
             .padding()
+            Spacer()
+        }
     }
 }
 
